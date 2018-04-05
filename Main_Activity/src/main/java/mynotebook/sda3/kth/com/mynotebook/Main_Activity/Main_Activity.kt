587d41782273e08a44618a26bc7a398444d09133
qaisarm <qmphy99@gmail.com>
2018-04-05 09:33:40 +0200

    first commit

---
+++ Main_Activity/src/main/java/mynotebook/sda3/kth/com/mynotebook/Main_Activity/Main_Activity.kt
@@ -0,0 +1,14 @@
+package mynotebook.sda3.kth.com.mynotebook.Main_Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_)
+        //btnSave.setOnClickListener {
+        //}
+        }
+}
